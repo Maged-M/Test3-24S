089a7a839973a692b4a4da6a42fd5287a9a28210
Maged-M <Maged82002@gmail.com>
2024-05-09 09:19:38 +0300

    Maged mohammed hamed alqthami 44205901 group:695

--- Test3-24S/Form1.cs
+++ Test3-24S/Form1.cs
@@ -6,5 +6,29 @@ namespace Test3_24S
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sum = (string)this.textBox1.Text;
+            string count = (string)this.textBox1.Text;
+            string average = (string)this.textBox1.Text;
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox1.Text);
+            double z = double.Parse(textBox1.Text);
+            z = x + y;
+            z = x / y;
+
+            
+          
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox2.Text);
+            if (x < 0) ;
+        }
     }
 }
